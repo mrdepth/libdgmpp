88fb69de7aa2d32cf1f690ea31ca1b6231d94c7e
mrdepth <shimanski.artem@gmail.com>
2012-02-20 15:06:18 -0100

    Database init script

---
+++ dbinit/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cd ./dump
+./datadump.py
+cd ..
+rm *.sqlite
+sqlite3 ./database.sqlite ".read init.sql"
+sqlite3 ./database.sqlite ".read eufe/fixes.sql"
+./compiler database.sqlite ./
+sqlite3 ./database.sqlite ".read eufeInit.sql"
+sqlite3 ./eufe.sqlite ".read dgmCompiledEffects.sql"
+rm ./database.sqlite
