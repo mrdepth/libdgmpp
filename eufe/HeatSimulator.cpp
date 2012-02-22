e5e1f195a5d562129d9b7feb81a1309283b28209
mrdepth <shimanski.artem@gmail.com>
2012-02-22 17:14:29 -0100

    Heat simulator added

---
+++ eufe/HeatSimulator.cpp
@@ -0,0 +1,9 @@
+//
+//  HeatSimulator.cpp
+//  eufe
+//
+//  Created by Mr. Depth on 2/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ eufe/HeatSimulator.h
@@ -0,0 +1,14 @@
+//
+//  HeatSimulator.h
+//  eufe
+//
+//  Created by Mr. Depth on 2/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef eufe_HeatSimulator_h
+#define eufe_HeatSimulator_h
+
+
+
+#endif
